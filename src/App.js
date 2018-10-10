fcaa546789f794e619b53a7c86c556ddaa9383db
arusu <arusu@sehcomp.ca>
2018-10-10 11:44:26 -0400

    Added a react transition pacakge and set up a little demo of how transitions can be done without having to always have an element mounted to the dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Transition from 'react-transition-group/Transition';
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -7,7 +8,8 @@ import List from "./components/List/List";
 
 class App extends Component {
   state = {
-    modalIsOpen: false
+    modalIsOpen: false,
+    showBlock: false
   }
 
   showModal = () => {
@@ -18,12 +20,31 @@ class App extends Component {
     this.setState({modalIsOpen: false})
   }
 
+  toggleBlock = () => {
+    this.setState(prevState => ({showBlock: !prevState.showBlock}));
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        <Backdrop show={this.state.modalIsOpen} />
+        <button className="Button" onClick={this.toggleBlock}>Toggle</button>
+        <br />
+        <Transition in={this.state.showBlock} timeout={300} mountOnEnter unmountOnExit>
+          { state => (
+            <div style=
+              {{backgroundColor: "red",
+                width: 100,
+                height: 100,
+                margin: 'auto',
+                transition: 'opacity 1s ease-out',
+                opacity: state === 'exiting' || state === 'entering' ? 0 : 1
+            }} />
+          )}
+          
+        </Transition>
+        {this.state.modalIsOpen ? <Modal closed={this.closeModal} /> : null}
+        {this.state.modalIsOpen ? <Backdrop /> : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
